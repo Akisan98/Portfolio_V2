4eb1c8e26e3f4be131f1c4158391fb77c2ccd0de
Akisan98 <51326806+Akisan98@users.noreply.github.com>
2020-08-20 21:28:14 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,55 @@
+const slide = document.querySelector('.overlay');
+slide.style.animation = "unset";
+
+setTimeout(function(){
+    gsap.fromTo(slide, .35, {transform: "scaleX(1)"}, {transform: "scaleX(0)"})
+}, 500);
+
+/* iOS Detection */
+function isIOSDevice(){
+    return !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+}
+if(isIOSDevice()){
+    console.log("I am an IOS device!");
+    alert("IOS");
+} else {
+    console.log("I am NOT an IOS device!");
+    alert("NOT IOS");
+}
+
+
+const links = document.querySelectorAll("a");
+
+if(!isIOSDevice()){
+    links.forEach(function(current){
+        current.addEventListener('click', function(event) {
+          const isTargetBlank = event.target.target === "_blank";
+          event.preventDefault();
+          var url = this.getAttribute("href");
+          
+          if (url != null && isTargetBlank == false)
+          {
+            gsap.fromTo(slide, 0.35,  {transform: "scaleX(0)"}, {transform: "scaleX(1)"})
+            setTimeout(function(){
+                window.location = url;
+            }, 350);
+          } else if (url != null && isTargetBlank == true) {
+            window.open(url)
+          }
+        })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
 // Minor Improvments bug brings in other bugs
 
 /*function adjustCSS(screenWidth) {
@@ -38,46 +90,7 @@ window.addEventListener('load', (event) => {
     });
   });*/
 
-const slide = document.querySelector('.overlay');
-slide.style.animation = "unset";
 
-setTimeout(function(){
-    gsap.fromTo(slide, .35, {transform: "scaleX(1)"}, {transform: "scaleX(0)"})
-}, 500);
-
-
-
-const links = document.querySelectorAll("a");
-
-links.forEach(function(current){
-  current.addEventListener('click', function(event) {
-    const isTargetBlank = event.target.target === "_blank";
-    event.preventDefault();
-    var url = this.getAttribute("href");
-    
-    if (url != null && isTargetBlank == false)
-    {
-      gsap.fromTo(slide, 0.35,  {transform: "scaleX(0)"}, {transform: "scaleX(1)"})
-      setTimeout(function(){
-          window.location = url;
-      }, 350);
-    } else if (url != null && isTargetBlank == true) {
-      window.open(url)
-    }
-  })
-})
-
-/* iOS Detection */
-function isIOSDevice(){
-    return !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
- }
- if(isIOSDevice()){
-     console.log("I am an IOS device!");
-     alert("IOS");
- } else {
-    console.log("I am NOT an IOS device!");
-    alert("NOT IOS");
- }
 
 
 /* window.onbeforeunload = function(event)
